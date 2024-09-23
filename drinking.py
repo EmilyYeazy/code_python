6a9beebd0bad95d0e7858535d45ad74c80ea4295
EmilyYeazy <emilyyan200788@gmail.com>
2024-09-23 08:48:45 -0400

    Introductory Python Program

---
+++ drinking.py
@@ -0,0 +1,11 @@
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+year = int(input("Which year you are borned in: "))
+age = 2024-year
+print(f"Hello {first_name} {last_name}.")
+print(f"You are {age} years old")
+
+if age>=19:
+    print("You are old enough to drink")
+else:
+    print("You are not old enough to drink")
