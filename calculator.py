83b8c0d8aedac5ccf1c2c5ce0108260505c67f10
EmilyYeazy <emilyyan200788@gmail.com>
2024-09-30 20:03:18 -0400

    first version of calculator

--- calculator.py
+++ calculator.py
@@ -1,14 +1,11 @@
 # Program Goal: Build a simple python program that can add, subtract, multiply and divide two numbers
 
-# Functional Decompotion:
-# 1. Obtain a way to grab two numbers
-# 2. Obtain a way to choose either add, subtract, multiply and divide
-# 3. Do the wanted calculation with the given two numbers
-# 4. Output the calculation/result
+#Program Weakness -> Can only do one calculation, then the program finishes
 
 print("Welcome to our calculator app!")
 
 # 1. Create a way to grab 2 numbers
+#Weakness -> crashes when the user inputs non integer values, it should also handle decimal numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
@@ -16,6 +13,7 @@ num2 = int(input("Enter the second number: "))
 choice = input("Choose either add, subtract, multiply, or devide: ")
 
 # 3. Complete the wanted calculation
+#Weakness -> case sensitive, add in support for + sign, create a menu type user interface
 if choice == "add":
     print(num1 + num2)
 elif choice == "subtract":
@@ -23,4 +21,5 @@ elif choice == "subtract":
 elif choice == "multiply":
     print(num1 * num2)
 elif choice == "devide":
+#Weakness -> can't devide by zero
     print(num1 / num2)
